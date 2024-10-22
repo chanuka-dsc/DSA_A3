79788288b0d732fe3fcadaf7405fb39d6eadd9a3
chanuka-dsc <85240799+chanuka-dsc@users.noreply.github.com>
2024-10-22 09:26:00 +0200

    complete

--- ALGraph.py
+++ ALGraph.py
@@ -3,23 +3,29 @@ class ALGraph:
     def __init__(self, n_lst) -> None:
         self.adj = {n: [] for n in n_lst}
 
+    # method to add an edge to the CONNECTED graph
     def add_edge(self, i: int, j):
         if j in self.adj.keys() and j not in self.adj.get(i):
+            # Both cases are added to cover the connected graph behavior
             self.adj.get(i).append(j)
             self.adj.get(j).append(i)
             return True
         return False
 
+    # method to remove and edge from the CONNECTED graph
     def remove_edge(self, i: int, j):
         if j in self.adj.keys() and j in self.adj.get(i):
+            # Both cases are added to cover the connected graph behavior
             self.adj.get(i).remove(j)
             self.adj.get(j).remove(i)
             return True
         return False
 
+    # method to add a node to the graph
     def add_node(self, n):
         self.adj[n] = []
 
+    # method to remove a node to the graph
     def remove_node(self, n):
 
         for lst in self.adj.values():
--- quick_sort.py
+++ quick_sort.py
@@ -1,5 +1,4 @@
 from Data_Structures.DoublyLinkedList import DoublyLinkedList
-from Data_Structures.Nodes.DLLNode import DLLNode
 
 arr = [2, 5, 1, 3, 7, 6, 9, 8, 10, 4]
 
@@ -7,6 +6,7 @@ lst = DoublyLinkedList()
 lst.append_array(arr)
 
 
+# quick sort algorithm that sorted the elements in an ascending order
 def quick_sort(lst: DoublyLinkedList, start: int, end: int):
     if end > start:
         i = _partition(lst, start, end)
@@ -14,18 +14,19 @@ def quick_sort(lst: DoublyLinkedList, start: int, end: int):
         quick_sort(lst, i + 1, end)
 
 
+# broke the logic down separately to make it easier to debug
 def _partition(lst: DoublyLinkedList, start: int, end: int):
     pivot = lst._get_node_by_i(end)
     i = start
-    j = end - 1
+    j = end - 1  # because the end is the pivot
 
     while i < j:
-        while i < end and lst._get_node_by_i(i).val < pivot.val:
+        while i <= j and lst._get_node_by_i(i).val < pivot.val:
             i += 1
-        while j > start and lst._get_node_by_i(j).val > pivot.val:
+        while i <= j and lst._get_node_by_i(j).val > pivot.val:
             j -= 1
 
-        if i < j:
+        if i <= j:
             i_node = lst._get_node_by_i(i)
             j_node = lst._get_node_by_i(j)
             i_node.val, j_node.val = j_node.val, i_node.val
@@ -37,5 +38,31 @@ def _partition(lst: DoublyLinkedList, start: int, end: int):
     return i
 
 
+print("\n\n Test 1 : random list\n\n")
+
 quick_sort(lst, 1, 10)
 lst.print_lst()
+
+print("\n\n Test 2 : list in descending order\n\n")
+arr_2 = [6, 5, 4, 3, 2, 1, 0]
+lst_2 = DoublyLinkedList()
+lst_2.append_array(arr_2)
+
+quick_sort(lst_2, 1, 7)
+lst_2.print_lst()
+
+print("\n\n Test 3 : All non positive and with two instance of the same value\n\n")
+arr_3 = [-2, -99, -15, 0, -12, -12, -1]
+lst_3 = DoublyLinkedList()
+lst_3.append_array(arr_3)
+
+quick_sort(lst_3, 1, 7)
+lst_3.print_lst()
+
+"""
+    Here I tried to follow the algorithm given in the slides but, I was not able to get some cases to work properly.
+    Afterwards I found this in the python book page 559 which had a different approach and
+    here the difference was instead of switching for each instance between the two sides it finds two larger and smaller values
+    the switch the two. Then when the two pointers pass each other we know that we have gone through all the elements. 
+    Then we just have to move the pivot to the correct position. 
+"""
